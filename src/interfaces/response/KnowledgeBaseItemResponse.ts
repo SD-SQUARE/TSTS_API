199cda9de6536551fdfcea94110bf2da55719daa
Esmailashraf <esmail5mostafa2@gmail.com>
2026-01-18 21:30:10 +0200

    finish knowledge base

---
+++ src/interfaces/response/KnowledgeBaseItemResponse.ts
@@ -0,0 +1,9 @@
+export interface KnowledgeBaseItemResponse {
+  id: string;
+  title: string;
+  description: string;
+  specialization: string;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
