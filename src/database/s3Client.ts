e4d665d98876bcb0f8a7c40bc14050369f729cf4
AhmedElSenaty <ahmedmohamedomarelsenaty@gmail.com>
2026-02-27 03:15:53 +0200

    [add]: ca cert to minio

--- src/database/s3Client.ts
+++ src/database/s3Client.ts
@@ -1,16 +1,24 @@
-
+import fs from "fs";
+import https from "https";
 import { S3Client } from "@aws-sdk/client-s3";
 
+
+
+const caCert = fs.readFileSync(process.env.MINIO_CA_CERT || "ca.crt");
 const ENDPOINT = `${process.env.MINIO_PROTOCOL}://${process.env.MINIO_HOST}:${process.env.MINIO_API_PORT}`;
 const ENDPOINT_FALLBACK = "http://localhost:9000";
 
 export const s3Client = new S3Client({
-  endpoint: ENDPOINT ?? ENDPOINT_FALLBACK, 
+  endpoint: ENDPOINT ?? ENDPOINT_FALLBACK,
   region: process.env.MINIO_REGION || "us-east-1",
   credentials: {
     accessKeyId: process.env.MINIO_USER || "minioadmin",
     secretAccessKey: process.env.MINIO_PASSWORD || "minioadmin123",
   },
-  forcePathStyle: true, // required for MinIO
+  forcePathStyle: true,
+  requestHandler: new (require("@aws-sdk/node-http-handler").NodeHttpHandler)({
+    httpsAgent: new https.Agent({
+      ca: caCert, // trust mkcert CA
+    }),
+  }),
 });
-
